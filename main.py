36a0c863f6695d8eca0f01bd0821c8abd589ff49
Vaibhav Vijay Nayak <vaibhavvijayn@sahaj.ai>
2026-01-23 10:38:59 +0530

    add docker compose for driver and executor

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
 from pyspark.sql import SparkSession
 from constants import dataset_path
 
-spark = SparkSession.builder.master("local").appName("FHV").getOrCreate()
+spark = SparkSession.builder.master("spark://127.0.0.1:7077").appName("FHV").getOrCreate()
 
 df = spark.read.parquet(dataset_path)
 
-df.printSchema()
+df.groupby("PULocationID").count().describe().show()
