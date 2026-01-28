1d9c375063d061cde2e4190fa642385994a0c9ce
Vaibhav Vijay Nayak <vaibhavvijayn@sahaj.ai>
2026-01-28 13:23:28 +0530

    use the constants from environement variables

--- config.py
+++ config.py
@@ -1,13 +1,15 @@
 from pyspark.sql import SparkSession
+import os
 
-MINIO_ENDPOINT = "http://minio:9000"
-MINIO_ACCESS_KEY = "minioadmin"
-MINIO_SECRET_KEY = "minioadmin"
-BUCKET_NAME = "parquet-data"
+DRIVER_URL = os.getenv("DRIVER_URL", "local[3]")
+MINIO_ENDPOINT = os.getenv("MINIO_ENDPOINT", "http://localhost:9000")
+MINIO_ACCESS_KEY = os.getenv("MINIO_ACCESS_KEY", "minioadmin")
+MINIO_SECRET_KEY = os.getenv("MINIO_SECRET_KEY", "minioadmin")
+BUCKET_NAME = os.getenv("BUCKET_NAME", "parquet-data")
 
 spark = (
     SparkSession.builder
-    .appName("MinIOParquetRead")
+    .appName("High Volume Taxi data analysis")
     .config("spark.jars.packages", "org.apache.hadoop:hadoop-aws:3.3.4,com.amazonaws:aws-java-sdk-bundle:1.12.262")
     .config("spark.hadoop.fs.s3a.aws.credentials.provider", "org.apache.hadoop.fs.s3a.SimpleAWSCredentialsProvider")
     .config("spark.hadoop.fs.s3a.endpoint", MINIO_ENDPOINT)
@@ -19,8 +21,6 @@ spark = (
     .config("spark.hadoop.fs.s3a.connection.timeout", "60000")
     .config("spark.hadoop.fs.s3a.threads.keepalivetime", "60")
     .config("spark.hadoop.fs.s3a.path.style.access", True)
-    .master("spark://spark-driver:7077")
+    .master(DRIVER_URL)
     .getOrCreate()
 )
-
-# spark = SparkSession.builder.master("spark://127.0.0.1:7077").appName("FHV").getOrCreate()
