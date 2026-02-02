87ebbce18305762e286345d5def4d349f225f3e6
Vaibhav Vijay Nayak <vaibhavvijayn@sahaj.ai>
2026-02-02 15:47:19 +0530

    increase core for executors

--- scripts/submit.sh
+++ scripts/submit.sh
@@ -7,5 +7,9 @@
   --conf spark.executor.instances=2 \
   --conf spark.kubernetes.container.image=spark-job:v1 \
   --conf spark.kubernetes.container.image.pullPolicy=Never \
+  --conf spark.executor.cores=2 \
+  --conf spark.executor.memory=1g \
+  --conf spark.driver.memory=512m \
+  --conf spark.kubernetes.submission.waitAppCompletion=false \
   --py-files local:///app/jobs.zip \
   local:///app/main.py
